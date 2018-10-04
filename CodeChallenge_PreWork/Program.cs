b026c435bf0be93cdce406227e097b62c78f2f5c
Danul De Leon <danul.deleon@gmail.com>
2018-10-04 13:25:27 -0700

    Add project files.

---
+++ CodeChallenge_PreWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeChallenge_PreWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
