20ea03b618071ed66e00f27ebb18634322ab41ab
Danul De Leon <danul.deleon@gmail.com>
2018-10-04 14:50:32 -0700

    Set up method

--- CodeChallenge_PreWork/Program.cs
+++ CodeChallenge_PreWork/Program.cs
@@ -11,8 +11,11 @@ namespace CodeChallenge_PreWork
 
             //Console.WriteLine(IsLeapYear(1900));
 
-            int[] sequence = { 0, -7, 0 };
-            Console.WriteLine(IsPerfect(sequence));
+            //int[] sequence = { 0, -7, 0 };
+            //Console.WriteLine(IsPerfect(sequence));
+
+            int[,] myArray = new int[3, 5] { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 } };
+            Console.WriteLine(SumArray(myArray));
         }
 
         static int CheckScore(int[] numArray, int number)
@@ -66,6 +69,12 @@ namespace CodeChallenge_PreWork
                 return "Yes";
             }
             return "No";
+        }
+
+        static int[] SumArray(int[,] multiDimArray)
+        {
+            int[] thisArray = { 1, 2, 3, 4 };
+            return thisArray;
         }
     }
 }
